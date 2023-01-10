2537dc921225c98802f7372536c26245f01d8291
Florian Morello <arsonik@me.com>
2023-01-10 21:21:47 +0100

    log err

--- src/calypshome.ts
+++ src/calypshome.ts
@@ -7,7 +7,7 @@ export class CalypsHome {
     lastUpdate?: number;
 
     constructor(private auth: { username: string; password: string }, public readonly log: Logger) {
-        this.log.info('CalypsHome init');
+        this.log.info(`Calyps'Home init with ${auth.username}/${'*'.repeat(auth.password.length)}`);
     }
 
     login() {
@@ -27,46 +27,55 @@ export class CalypsHome {
                 _password: this.auth.password,
                 _submit: 'Sign in',
             }).toString(),
-        }).then((response) => {
-            this.sessionId = response.headers.get('set-cookie')?.match(/JSESSIONID=([^;]+)/)?.[1];
-            if (!response.headers.get('location')) {
-                throw new Error('Login failed');
-            }
-            this.log.debug('Logged in', this.sessionId);
-        });
+        })
+            .then((response) => {
+                this.sessionId = response.headers.get('set-cookie')?.match(/JSESSIONID=([^;]+)/)?.[1];
+                if (!response.headers.get('location')) {
+                    throw new Error('Login failed');
+                }
+                this.log.debug('Logged in', this.sessionId);
+            })
+            .catch((e) => {
+                this.log.error('Login failed', e);
+            });
     }
 
     async devices(): Promise<DeviceType[]> {
-        return this.login().then(() =>
-            fetch(`${this.url}/ajax`, {
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json',
-                    Cookie: `JSESSIONID=${this.sessionId}`,
-                },
-            })
-                .then((x) => x.json())
-                .then((data) => data[0][1]['objects'] as { id: number; gw: string; statuss: { statusname: string; status: string }[] }[])
-                .then((x) => {
-                    this.lastUpdate = new Date().getTime();
-                    return x.map((g) => {
-                        const kv = g.statuss.reduce((acc, s) => {
-                            const m = s.statusname.match(/\/([^/]+)$/);
-                            if (m) {
-                                acc[m[1]] = s.status;
-                            }
-                            return acc;
-                        }, {} as DeviceType['kv']);
-                        return {
-                            id: g.id,
-                            gw: g.gw,
-                            kv,
-                            name: kv['__user_name'],
-                            manufacturer: kv['manufacturer_name'],
-                        };
-                    });
+        return this.login()
+            .then(() =>
+                fetch(`${this.url}/ajax`, {
+                    method: 'POST',
+                    headers: {
+                        Accept: 'application/json',
+                        Cookie: `JSESSIONID=${this.sessionId}`,
+                    },
                 })
-        );
+                    .then((x) => x.json())
+                    .then((data: any) => data[0][1]['objects'] as { id: number; gw: string; statuss: { statusname: string; status: string }[] }[])
+                    .then((x) => {
+                        this.lastUpdate = new Date().getTime();
+                        return x.map((g) => {
+                            const kv = g.statuss.reduce((acc, s) => {
+                                const m = s.statusname.match(/\/([^/]+)$/);
+                                if (m) {
+                                    acc[m[1]] = s.status;
+                                }
+                                return acc;
+                            }, {} as DeviceType['kv']);
+                            return {
+                                id: g.id,
+                                gw: g.gw,
+                                kv,
+                                name: kv['__user_name'],
+                                manufacturer: kv['manufacturer_name'],
+                            };
+                        });
+                    })
+            )
+            .catch((e) => {
+                this.log.error('Failed to get devices', e);
+                return [];
+            });
     }
 
     action(object: { id: number; gw: string }, action: 'STOP' | 'CLOSE' | 'OPEN' | 'LEVEL' | 'TILT', args?: string) {
@@ -77,23 +86,28 @@ export class CalypsHome {
             args: args ?? '',
         });
         this.log.debug('ACTION', sp);
-        return this.login().then(() =>
-            fetch(`${this.url}/ihm`, {
-                method: 'POST',
-                headers: {
-                    Cookie: `JSESSIONID=${this.sessionId}`,
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: sp.toString(),
-            }).then((response) => {
-                if (response.status === 401) {
-                    this.log.debug('Session expired');
-                    this.sessionId = undefined;
-                    return this.action(object, action, args);
-                }
-                this.log.debug('ACTION result', response.status, response.statusText);
-                return true;
-            })
-        );
+        return this.login()
+            .then(() =>
+                fetch(`${this.url}/ihm`, {
+                    method: 'POST',
+                    headers: {
+                        Cookie: `JSESSIONID=${this.sessionId}`,
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    body: sp.toString(),
+                }).then((response) => {
+                    if (response.status === 401) {
+                        this.log.debug('Session expired');
+                        this.sessionId = undefined;
+                        return this.action(object, action, args);
+                    }
+                    this.log.debug('ACTION result', response.status, response.statusText);
+                    return true;
+                })
+            )
+            .catch((e) => {
+                this.log.error('Failed to send action', e);
+                return [];
+            });
     }
 }
